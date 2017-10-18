8f5b38875176bb9fc725731d8ae2383f9d2fa9ed
Joseph Romero <jromero361980@gmail.com>
2017-10-18 17:09:53 +0000

    help-3

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -23,4 +23,23 @@ RSpec.describe AdvertisementsController, type: :controller do
       expect(assigns(:advertisements)).to eq([my_ad])
     end
   end
+  
+  describe "GET #show" do 
+    it "returns http success" do 
+      get :show, {id: my_ad.id}
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #show view" do
+      get :show, {id: my_ad.id}
+      expect(response).to render_template :show
+    end
+    
+    it "assigns my_ad to @advertisement" do
+      get :show, {id: my_ad.id}
+      expect(assigns(:advertisement)).to eq(my_ad)
+    end
+  end
+  
+  
 end

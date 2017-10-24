c4aa84762341c359c85ff82eddf07dad6e82170b
Joseph Romero <jromero361980@gmail.com>
2017-10-24 16:07:04 +0000

    help-2

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -1,10 +1,59 @@
 class SponsoredPostsController < ApplicationController
-  def show
+  def index
+    @sponsored_posts = SponsoredPost.all
   end
-
+  
+  def show 
+    @sponsored_post = SponsoredPost.find(params[:SponsoredPost_id])
+  end
+  
   def new
+    @sponsored_post = SponsoredPost.new
   end
-
+  
+  def create
+    @sponsored_post = SponsoredPost.new
+    @sponsored_post.name = params[:SponsoredPost][:name]
+    @sponsored_post.description = params[:SponsoredPost][:description]
+    @sponsored_post.public = params[:SponsoredPost][:public]
+ 
+    if @sponsored_post.save
+     redirect_to @sponsored_post, notice: "SponsoredPost was saved successfully."
+    else
+     flash.now[:alert] = "Error creating SponsoredPost. Please try again."
+     render :new
+    end
+  end
+  
   def edit
+     @sponsored_post = SponsoredPost.find(params[:SponsoredPost_id])
+  end
+  
+  def update
+     @sponsored_post = SponsoredPost.find(params[:SponsoredPost_id])
+ 
+     @sponsored_post.name = params[:SponsoredPost][:name]
+     @sponsored_post.description = params[:SponsoredPost][:description]
+     @sponsored_post.public = params[:SponsoredPost][:public]
+ 
+     if @sponsored_post.save
+        flash[:notice] = "SponsoredPost was updated."
+       redirect_to @sponsored_post
+     else
+       flash.now[:alert] = "Error saving SponsoredPost. Please try again."
+       render :edit
+     end
+  end
+ 
+  def destroy
+     @sponsored_post = SponsoredPost.find(params[:SponsoredPost_id])
+ 
+     if @sponsored_post.destroy
+       flash[:notice] = "\"#{@sponsored_post.name}\" was deleted successfully."
+       redirect_to action: :index
+     else
+       flash.now[:alert] = "There was an error deleting the SponsoredPost."
+       render :show
+     end
   end
 end
--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -19,18 +19,78 @@ RSpec.describe SponsoredPostsController, type: :controller do
      end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "GET new" do
+     it "returns http success" do
+       get :new
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #new view" do
+       get :new
+       expect(response).to render_template :new
+     end
+ 
+     it "initializes @topic" do
+       get :new
+       expect(assigns(:topic)).not_to be_nil
+     end
+   end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "GET edit" do
+     it "returns http success" do
+       get :show, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #edit view" do
+       get :show, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       expect(response).to render_template :edit
+     end
+ 
+     it "assigns topic to be updated to @topic" do
+       get :show, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       topic_instance = assigns(:topic)
+ 
+       expect(topic_instance.id).to eq my_topic.id
+       expect(topic_instance.name).to eq my_topic.name
+       expect(topic_instance.description).to eq my_topic.description
+     end
+   end
+   
+   describe "PUT update" do
+     it "updates sponsored_post with expected attributes" do
+       new_name = RandomData.random_sentence
+       new_description = RandomData.random_paragraph
+ 
+       put :update, params: { topic_id: my_topic.id, topic: { name: new_name, description: new_description } }
+ 
+       updated_topic = assigns(:topic)
+       expect(updated_topic.id).to eq my_topic.id
+       expect(updated_topic.name).to eq new_name
+       expect(updated_topic.description).to eq new_description
+     end
+ 
+     it "redirects to the updated sponsored_post" do
+       new_name = RandomData.random_sentence
+       new_description = RandomData.random_paragraph
+ 
+       put :update, params: { topic_id: my_topic.id, topic: { name: new_name, description: new_description } }
+       expect(response).to redirect_to my_topic
+     end
+   end
+   
+   describe "DELETE destroy" do
+     it "deletes the topic" do
+       delete :destroy, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       count = SponsoredPost.where({ topic_id: my_topic.id, id: sponsored_post.id })
+       expect(count).to eq 0
+     end
+   
+     it "redirects to topics index" do
+       delete :destroy, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       expect(response).to redirect_to sponsored_post_path
+     end
+   end
+   
 
 end

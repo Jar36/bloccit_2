5e71b2cba8dff425c48a0173c09bd85dbe3d903f
Joseph Romero <jromero361980@gmail.com>
2017-11-07 01:13:56 +0000

    add association between users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 require 'random_data'
 # Post.destroy_all
 # Comment.destroy_all
+# Create Users
+
+ 5.times do
+  User.create!(
+   name: RandomData.random_name,
+   email: RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
 
 # Create Topics
  15.times do
@@ -16,6 +26,7 @@ require 'random_data'
  # #1
    Post.create!(
  # #2
+     user: users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -33,6 +44,13 @@ require 'random_data'
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+  email: 'jromero45@hotmail.com', 
+  password: 'helloworld'
+ )
+ 
+ puts "#{User.count} users created"
  puts "Seed finished"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"

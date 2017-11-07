56df05beb7cbe3979a9b9c13bb0a78ac93aa5fd9
Joseph Romero <jromero361980@gmail.com>
2017-11-07 04:08:37 +0000

    add user roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,10 +44,19 @@ require 'random_data'
    )
  end
  
- user = User.first
- user.update_attributes!(
-  email: 'jromero45@hotmail.com', 
-  password: 'helloworld'
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
  )
  
  puts "#{User.count} users created"
